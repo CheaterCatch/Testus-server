80682764f572a1080a868eeb228a56bfafcdeff6
Chanran33 <k55416325@gmail.com>
2021-07-14 14:30:48 +0900

    ormconfig.js 수정, .gitignore 수정

--- ormconfig.js
+++ ormconfig.js
@@ -2,11 +2,11 @@ const SnakeNamingStrategy = require("typeorm-naming-strategies").SnakeNamingStra
 
 module.exports = {
     "type": "mysql",
-    "host": "api.eastflag.co.kr",
+    "host": "develop-catcher-db.cuplkaqohecx.ap-northeast-2.rds.amazonaws.com",
     "port": 3306,
-    "username": "eastflag",
-    "password": "dklee987",
-    "database": "eastdb",
+    "username": "catcher",
+    "password": "catcher5",
+    "database": "catcherdb",
     "synchronize": true,
     "logging": true,
     "entities": [
